10d0e70da37d5f7ea52eebd5d77c30a44a78ef5c
Manhim Yeung <14059614@students.southwales.ac.uk>
2021-04-26 16:54:54 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueWordUnscramble = true;
+            do
+            {
+                Console.Write("Please enter the option");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("Enter scrambled word file name: ");
+                        ExecuteScrambledWordsInFileScenario();
+                        break;
+                    case "M":
+                        Console.Write("Enter scrambled words manually: ");
+                        ExecuteScrambledWordsManualEntryScenario();
+                        break;
+                    default:
+                        Console.Write("Option was not recognised. ");
+                        break;
+                }
+
+                var continueWordUnscrambleDecision = string.Empty;
+                do
+                {
+                    Console.WriteLine("Do you want to continue?");
+                    continueWordUnscrambleDecision = (Console.ReadLine() ?? string.Empty);
+                } while (!continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) &&
+                         !continueWordUnscrambleDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+            } while (continueWordUnscramble);
+        }
+
+        private static void ExecuteScrambledWordsInFileScenario()
+        {
+            throw new NotImplementedException();
+        }
+        private static void ExecuteScrambledWordsManualEntryScenario()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
